f5c9d0e48c3cd7bb19cb0230d11e823bf32f3506
Ross Beazley <ross@rossbeazley.co.uk>
2018-10-27 20:58:34 +0200

    Use www sources in deployment

--- dockerize-busybox-httpd.sh
+++ dockerize-busybox-httpd.sh
@@ -2,8 +2,7 @@
 set -e
 
 dockerize -t hypriot/rpi-busybox-httpd \
-   --add-file index.html /www/ \
-   --add-file pi_armed_with_docker.jpg /www/ \
+   -a www /var/www \
    --entry '/bin/busybox' \
    --cmd 'httpd -f -p 80 -h /www' \
    /bin/busybox
